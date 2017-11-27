916ba6218d19b3e52df45c46da7cddf262bfe13d
Neil Singh <Neil@Neils-MacBook-Pro.local>
2017-11-26 22:33:41 -0600

    MP3.1 base

--- ece408_src/new-forward.cuh
+++ ece408_src/new-forward.cuh
@@ -9,7 +9,8 @@ namespace mxnet
 namespace op
 {
 
-
+#define TILE_WIDTH  1
+    
 template<typename gpu, typename DType>
 __global__ void forward_kernel(DType *y, const DType *x, const DType *k, const int B, const int M, const int C, const int H, const int W, const int K) {
 
@@ -22,45 +23,65 @@ __global__ void forward_kernel(DType *y, const DType *x, const DType *k, const i
 
     const int H_out = H - K + 1;
     const int W_out = W - K + 1;
-    (void)H_out; // silence declared but never referenced warning. remove this line when you start working
-    (void)W_out; // silence declared but never referenced warning. remove this line when you start working
+    
     #define y4d(i3,i2,i1,i0) y[(i3) * (M * H_out * W_out) + (i2)*(H_out * W_out) + (i1)*(W_out) + i0]
     #define x4d(i3,i2,i1,i0) x[(i3) * (C * H * W) + (i2)*(H * W) + (i1)*(W) + i0]
     #define k4d(i3,i2,i1,i0) k[(i3) * (C * K * K) + (i2)*(K * K) + (i1)*(K) + i0]
 
-    /*
-        Your code here!
-    */
+    int W_grid = ceil(W_out / (float)TILE_WIDTH);
+    int H_grid = ceil(H_out / (float)TILE_WIDTH);
+    int n, m, h, w, c, p, q;
+    n = blockIdx.x;
+    m = blockIdx.y;
+    h = blockIdx.z / W_grid + threadIdx.y;
+    w = blockIdx.z % W_grid + threadIdx.x;
+    
+    float acc = 0;
+    for (c = 0; c < C; c++) {
+        for (p = 0; p < K; p++) {
+            for (q = 0; q < K; q++) {
+                if (h+p < H && w+q < W)
+                    acc += x4d(n, c, h+p, w+q) * k4d(m, c, p, q);
+            }
+        }
+    }
+    y4d(n, m, h, w) = acc;
 
     #undef y4d
     #undef x4d
     #undef k4d
 }
 
-
-
-
 // This function is called by new-inl.h
 // Any code you write should be executed by this function
 template<typename gpu, typename DType>
 void forward(mshadow::Tensor<gpu, 4, DType> &y, const mshadow::Tensor<gpu, 4, DType> &x, const mshadow::Tensor<gpu, 4, DType> &w) {
-    
-
     // Use mxnet's CHECK_EQ to do assertions.
-    CHECK_EQ(0, 1) << "Missing an ECE408 GPU implementation!";
 
     // You'll probably need to launch kernels against the right stream to keep MXNet happy
-    // cudaStream_t s = y.stream_->stream_;
+    cudaStream_t s = y.stream_->stream_;
 
     // Extract the tensor dimensions into B,M,C,H,W,K
-    // ...
-
+    const int B = x.shape_[0];
+    const int M = y.shape_[1];
+    const int C = x.shape_[1];
+    const int H = x.shape_[2];
+    const int W = x.shape_[3];
+    const int K = w.shape_[3];
+    
+    int H_out = H - K + 1;
+    int W_out = W - K + 1;
+    
+    int W_grid = ceil(W_out / (float)TILE_WIDTH);
+    int H_grid = ceil(H_out / (float)TILE_WIDTH);
+    int Z = H_grid * W_grid;
+    
     // Set the kernel dimensions
-    // dim3 gridDim(0);
-    // dim3 blockDim(0);
-
+    dim3 blockDim(TILE_WIDTH, TILE_WIDTH, 1);
+    dim3 gridDim(B, M, Z);
+    
     // Call the kernel
-    // forward_kernel<gpu, DType><<<gridDim, blockDim, 0, s>>>(y.dptr_,x.dptr_,w.dptr_, B,M,C,H,W,K);
+    forward_kernel<gpu, DType><<<gridDim, blockDim, 0, s>>>(y.dptr_,x.dptr_,w.dptr_, B,M,C,H,W,K);
 
     // Use MSHADOW_CUDA_CALL to check for CUDA runtime errors.
     MSHADOW_CUDA_CALL(cudaDeviceSynchronize());
